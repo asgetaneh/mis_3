64e2e6d2379c23a52c9a8d43cfabb94a89cfd5cb
YishakZewdineh <yishakzewdineh@gmail.com>
2023-06-02 08:47:37 +0300

    reportingPeriodType create and index
    added locale column in migration

--- resources/views/app/reporting_period_types/form-inputs.blade.php
+++ resources/views/app/reporting_period_types/form-inputs.blade.php
@@ -1,3 +1,24 @@
 @php $editing = isset($reportingPeriodType) @endphp
+@foreach ($languages as $key => $lang)
+    <x-inputs.group class="col-sm-12">
+        <x-inputs.text name="{{ 'name' . $lang->locale }}" label="{{ 'Name in ' . $lang->name }}" maxlength="255"
+            placeholder="{{ 'name in ' . $lang->name }}" required></x-inputs.text>
+    </x-inputs.group>
 
+    {{-- <x-inputs.group class="col-sm-12">
+        <x-inputs.text name="{{ 'output' . $lang->locale }}" label="{{ 'Output in ' . $lang->name }}" maxlength="255"
+            placeholder="{{ 'output in ' . $lang->name }}" required></x-inputs.text>
+    </x-inputs.group>
+
+    <x-inputs.group class="col-sm-12">
+        <x-inputs.text name="{{ 'outcome' . $lang->locale }}" label="{{ 'Outcome in ' . $lang->name }}" maxlength="255"
+            placeholder="{{ 'outcome in ' . $lang->name }}" required></x-inputs.text>
+    </x-inputs.group> --}}
+
+    <x-inputs.group class="col-sm-12">
+        <x-inputs.textarea name="{{ 'description' . $lang->locale }}" label="{{ 'Description in ' . $lang->name }}"
+            maxlength="255" required>
+        </x-inputs.textarea>
+    </x-inputs.group>
+@endforeach
 <div class="row"></div>
