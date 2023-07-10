ec44f7de250fb12f3cc58d49d3936da339b5cca8
yizewdineh <yishak.sahilemariam@ju.edu.et>
2023-07-10 16:03:02 +0300

    fix sidebar offline collapse

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,14 @@
         <title>JU MIS</title>
 
         <!-- Scripts -->
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
         {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> --}}
         {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script> --}}
         {{-- <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script> --}}
-        <script src="{{ asset('assets/dist/js/adminlte.min.js') }}">
+        <script src="{{ asset('assets/dist/js/adminlte.min.js') }}"></script>
 
-        <script src="https://unpkg.com/alpinejs@3.10.2/dist/cdn.min.js" defer></script>
+        {{-- <script src="https://unpkg.com/alpinejs@3.10.2/dist/cdn.min.js" defer></script> --}}
+        <script src="{{ asset('assets/dist/js/alpine.min.js') }}"></script>
         <link rel="stylesheet" href="{{ asset('assets/plugins/select2/css/select2.min.css') }}">
         <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
 
@@ -28,7 +29,9 @@
         <!-- Styles -->
         {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
         {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css"> --}}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+        {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css"> --}}
+        <link rel="stylesheet" href="{{ asset('assets/dist/css/notyf.min.css') }}">
+
         <link rel="stylesheet" href="{{ asset('assets/dist/css/AdminLTE.min.css') }}">
 
         {{-- Calender Styles --}}
@@ -91,9 +94,11 @@
 
         @stack('scripts')
 
-        <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+        {{-- <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script> --}}
+        <script src="{{ asset('assets/dist/js/notyf.min.js') }}"></script>
         <script src="{{ asset('assets/plugins/select2/js/select2.full.min.js') }}"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+        <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
+        {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> --}}
 
         {{-- Calender Scripts --}}
         <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
