733af27f842e036929ad93a2f09a4ba4afcd1296
yizewdineh <yishak.sahilemariam@ju.edu.et>
2023-10-28 19:50:30 +0300

    emis student - overview partial fetch

--- resources/views/app/emis/student/overview/index.blade.php
+++ resources/views/app/emis/student/overview/index.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 @section('title', 'EMIS Student - Overview')
 
+@section('style')
+    <style>
+        /* Prevent any data making a break on the td tags */
+        td {
+            white-space: nowrap !important;
+        }
+    </style>
+@endsection
+
 @section('content')
     <div class="container-fluid">
         <div class="card">
@@ -66,13 +75,13 @@
                     <table class="table table-bordered table-hover">
                         <thead>
                             <tr>
+                                <th>#</th>
                                 <th class="text-left">
                                     student_national_id
                                 </th>
                                 <th class="text-left">
                                     student_institution_id
                                 </th>
-                                <th>
                                 <th>first_name
                                 </th>
                                 <th class="text-left">
@@ -124,6 +133,33 @@
                         <tbody>
                             @forelse($overviews as $key => $overview)
                                 <tr>
+                                    <td>{{ $key+1 }}</td>
+                                    <td>{{ $overview->national_id ?? '' }}</td>
+                                    <td>{{ $overview->student_id ?? '' }}</td>
+                                    <td>{{ $overview->first_name ?? '' }}</td>
+                                    <td>{{ $overview->first_name_lng ?? '' }}</td>
+                                    <td>{{ $overview->fathers_name ?? '' }}</td>
+                                    <td>{{ $overview->fathers_name_lng ?? '' }}</td>
+                                    <td>{{ $overview->grand_fathers_name ?? '' }}</td>
+                                    <td>{{ $overview->grand_fathers_name_lng ?? '' }}</td>
+                                    <td>{{ $overview->grand_fathers_name ?? '' }}</td>
+                                    <td>{{ $overview->last_name_lng ?? '' }}</td>
+                                    <td>{{ $overview->birth_date ?? '' }}</td>
+                                    <td>{{ $overview->place_of_birth ?? '' }}</td>
+                                    <td>{{ $overview->email_address ?? '' }}</td>
+                                    <td>{{ $overview->telephone ?? '' }}</td>
+                                    <td>{{ $overview->sex ?? '' }}</td>
+                                    <td>{{ $overview->country_code ?? '' }}</td>
+                                    <td>{{ $overview->country_code ?? '' }}</td>
+                                    <td>{{ $overview->state_code ?? '' }}</td>
+                                    <td>{{ $overview->zone_code ?? '' }}</td>
+                                    <td>{{ $overview->kebele ?? '' }}</td>
+                                    <td>{{ $overview->woreda_code ?? '' }}</td>
+                                    <td>{{ $overview->area_type ?? '' }}</td>
+                                    <td>{{ $overview->entrance_exam_id ?? '' }}</td>
+                                    <td>{{ $overview->student_tax_identification_number ?? '' }}</td>
+                                    <td>{{ $overview->student_national_system_id ?? '' }}</td>
+
                                 </tr>
                             @empty
                                 <tr>
