f55ce7f7e7c7c4e1e53da8c492e87d06c69eb794
yizewdineh <yishak.sahilemariam@ju.edu.et>
2023-12-13 22:05:24 +0300

    commented

--- resources/views/app/plan_accomplishments/view-planning-acc.blade.php
+++ resources/views/app/plan_accomplishments/view-planning-acc.blade.php
@@ -79,7 +79,7 @@
                                     <button class="btn btn-primary" value="search" name="search" type="submit">Filter</button>
                                     <span class="border border-right mx-3"></span>
                                     <button class="btn btn-info" value="pdf" name="pdf" type="submit">PDF</button>
-                                    <button class="btn btn-success" value="excel" name="excel" type="submit">Excel</button>
+                                    {{-- <button class="btn btn-success" value="excel" name="excel" type="submit">Excel</button> --}}
                                     {{-- <button class="btn btn-primary" value="word" name="word" type="submit">Word</button> --}}
                                 </div>
                             </div>
