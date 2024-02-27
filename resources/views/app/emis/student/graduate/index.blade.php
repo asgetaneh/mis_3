5718af51dd8639c3fea911eda367c596ad53be73
yizewdineh <yishak.sahilemariam@ju.edu.et>
2024-02-27 13:23:35 +0300

    grad filter

--- resources/views/app/emis/student/graduate/index.blade.php
+++ resources/views/app/emis/student/graduate/index.blade.php
@@ -20,54 +20,61 @@
             </div>
             <div class="card-body">
 
-                {{-- <div class="card card-outline card-primary collapsed-card">
-                <div class="card-header">
-                    <h3 class="card-title">Filter</h3>
+                <div class="card card-outline card-primary collapsed-card mb-3">
+                    <div class="card-header">
+                        <h3 class="card-title">Filter</h3>
 
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
-                                class="fas fa-plus"></i>
-                        </button>
-                    </div>
-                    <!-- /.card-tools -->
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body" style="display: none;">
-                    <form role="form" class="" method="post" action="">
-                        @csrf
-                        <div class="row m-0 p-0">
-                            <div class="">
-                                <label class="label" for="filters">Option 1:</label>
-                                <div>
-                                    <select class="form-control select2" name="name">
-                                        <option value="">Select</option>
-                                    </select>
-                                </div>
-
-                            </div>
-                            <div class="">
-                                <label class="" for="filters">Option 2:</label>
-                                <div>
-                                    <select class="form-control select2" name="name">
-                                        <option value="">Select</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="">
-                                <label class="label" for="action">Action</label>
-                                <div id="action">
-                                    <button class="btn btn-primary" value="filter" name="filter"
-                                        type="submit">Filter</button>
-                                    <span class="border border-right mx-3"></span>
-                                    <button class="btn btn-success" value="excel" name="excel" type="submit">Download
-                                        Excel</button>
-                                </div>
-                            </div>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i
+                                    class="fas fa-plus"></i>
+                            </button>
                         </div>
-                    </form>
+                        <!-- /.card-tools -->
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body" style="display: none;">
+                        <form role="form" class="" method="get" action="{{ url()->current() }}">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <label class="label" for="filters">Academic Year</label>
+                                    <div class="form-group">
+                                        <select class="form-control" name="academic_year">
+                                            <option {{ old('academic_year') == '' ? 'selected' : '' }} value=""
+                                                disabled>Select</option>
+                                            <option {{ old('academic_year') == '2023/24' ? 'selected' : '' }}
+                                                value="2023/24">2023/24</option>
+                                            <option {{ old('academic_year') == '2022/23' ? 'selected' : '' }}
+                                                value="2022/23">2022/23</option>
+                                            <option {{ old('academic_year') == '2021/22' ? 'selected' : '' }}
+                                                value="2021/22">2021/22</option>
+                                        </select>
+                                    </div>
+
+                                </div>
+                                {{-- <div class="">
+                                        <label class="" for="filters">Option 2:</label>
+                                        <div>
+                                            <select class="form-control select2" name="name">
+                                                <option value="">Select</option>
+                                            </select>
+                                        </div>
+                                    </div> --}}
+                                <div class="">
+                                    <label class="label" for="action">Action</label>
+                                    <div id="action">
+                                        <button class="btn btn-primary" value="filter" name="filter"
+                                            type="submit">Filter</button>
+                                        {{-- <span class="border border-right mx-3"></span> --}}
+                                        {{-- <button class="btn btn-success" value="excel" name="excel" type="submit">Download
+                                                Excel</button> --}}
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.card-body -->
                 </div>
-                <!-- /.card-body -->
-            </div> --}}
 
                 {{-- <button class="btn btn-success">Download Excel</button> --}}
 
@@ -118,7 +125,7 @@
                                  @php
                                      $nation_id = $nation_institute_id-> getNationalId($graduate->stud_id);
                                 @endphp
-                                <tr> 
+                                <tr>
                                     <td>{{ $key + 1 }}</td>
                                     <td>{{ $graduate->institution_code ?? '' }}
                                     </td>
